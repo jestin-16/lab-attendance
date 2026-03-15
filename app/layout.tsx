bd0cc222323f1b34479eb3fb7d611f4bad0f7a76
jestin16 <jestinshaji777@gmail.com>
2026-03-15 09:50:27 +0530

    changed the ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import "./globals.css";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
